917d3929476bc557b5f6cb411ff183afd4127522
shubham.naroji <shubham.naroji@infi-pre.com>
2023-05-22 12:55:41 +0530

    21346

--- src/UploadScorm.jsx
+++ src/UploadScorm.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import AdmZip from "adm-zip";
-import { saveAs } from "file-saver";
-import JSZip from "jszip";
 
 function UploadScorm() {
   const [file, setFile] = useState();
@@ -11,48 +8,29 @@ function UploadScorm() {
     setFile(e.target.files[0]);
   };
 
-  const saveFile = (filePath, fileData) => {
-    const blob = new Blob([fileData]);
-    saveAs(blob, filePath);
-  };
-
-  const handleFileUpload = () => {
-    if (file) {
-      const reader = new FileReader();
-
-      reader.onload = (event) => {
-        const zip = new JSZip();
-        const zipFile = event.target.result;
-
-        zip
-          .loadAsync(zipFile)
-          .then((zipContents) => {
-            zipContents.forEach((relativePath, zipEntry) => {
-              if (!zipEntry.dir) {
-                zipEntry.async("uint8array").then((fileData) => {
-                  // Store the file in a folder (e.g., 'unzipped') using the relative path
-                  const filePath = `/home/shubham/Desktop/Infipre/test/scorm_player/src/scormExtract/${relativePath}`;
-                  saveFile(filePath, fileData);
-                  // TODO: Save the file data or perform any other desired operations
-                  console.log(filePath, fileData);
-                });
-              }
-            });
-          })
-          .catch((error) => {
-            console.error("Error unzipping file:", error);
-          });
-      };
-
-      reader.readAsArrayBuffer(file);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = new FormData();
+    form.append("scormFile", file);
+    console.log(file);
+    axios
+      .post("http://localhost/postman/extract.php", form, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        window.location.href = "/showContents";
+      });
+    //form.append('file', file)
   };
 
   return (
     <div>
       <input type="file" accept=".zip" onChange={handleFileChange} />
 
-      <button type="button" onClick={handleFileUpload}>
+      <button type="button" onClick={handleSubmit}>
         Submit
       </button>
     </div>
