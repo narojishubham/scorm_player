ffa07bbf311cbf66fcf93cfb948034b3b3e55a94
shubham.naroji <shubham.naroji@infi-pre.com>
2023-05-22 15:23:58 +0530

    swaerusjkbasjhfihfksh

--- src/UploadScorm.jsx
+++ src/UploadScorm.jsx
@@ -1,11 +1,58 @@
 import React, { useState } from "react";
-
+import * as JSZip from "jszip";
 function UploadScorm() {
   const [file, setFile] = useState();
   const [extractedFiles, setExtractedFiles] = useState([]);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
+    console.log();
+  };
+
+  const handleDrop = async (e) => {
+    e.preventDefault();
+    const compressedFile = file;
+
+    if (!compressedFile) {
+      return;
+    }
+
+    try {
+      const zip = await JSZip.loadAsync(compressedFile);
+      const extractedFilePaths = [];
+
+      zip.forEach(async (relativePath, zipEntry) => {
+        if (!zipEntry.dir) {
+          const extractedFile = await zip.file(zipEntry.name).async("blob");
+          const srcFolderPath =
+            "/home/shubham/Desktop/Infipre/test/scorm_player/src/" +
+            file.name +
+            "/" +
+            zipEntry.name; // Target folder path in the src folder
+          const newFile = new File([extractedFile], srcFolderPath);
+          extractedFilePaths.push(newFile);
+
+          // Store the file in the src folder
+          const fileReader = new FileReader();
+          fileReader.onload = () => {
+            const fileData = fileReader.result;
+            const a = document.createElement("a");
+            a.href = fileData;
+            a.download = srcFolderPath;
+            a.style.display = "none";
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+          };
+          fileReader.readAsDataURL(file);
+        }
+        console.log("Unzipped");
+      });
+
+      setExtractedFiles(extractedFilePaths);
+    } catch (error) {
+      console.error("Error extracting files:", error);
+    }
   };
 
   const handleSubmit = (e) => {
@@ -30,9 +77,19 @@ function UploadScorm() {
     <div>
       <input type="file" accept=".zip" onChange={handleFileChange} />
 
-      <button type="button" onClick={handleSubmit}>
+      <button type="button" onClick={handleDrop}>
         Submit
       </button>
+      {extractedFiles.length > 0 && (
+        <div>
+          <h3>Extracted Files:</h3>
+          <ul>
+            {extractedFiles.map((file) => (
+              <li key={file.name}>{file.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
